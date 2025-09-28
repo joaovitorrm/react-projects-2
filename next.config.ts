558d04871cc4776c67cb9f1997ecb3d8ab9b612a
Joao <joao-morrudo@hotmail.com.br>
2025-09-28 19:49:10 -0300

    Test 2

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
+const repoName = "react-projects-2";
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig: NextConfig = {
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}/` : "",
   /* config options here */
 };
 
