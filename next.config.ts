6400b5161bf79c49efe32905ea1fd1afd921fdca
Joao <joao-morrudo@hotmail.com.br>
2025-09-28 16:48:11 -0300

    Fixing?3

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {unoptimized: true},
   basePath: "/react-projects-2",
   assetPrefix: "/react-projects-2/",
-  output: "export",  
+  output: "export",
   /* config options here */
 };
 
