0a7837405d19f8670cd90e1bf941cbba38bada42
Joao <joao-morrudo@hotmail.com.br>
2025-10-03 18:26:35 -0300

    nonogram cover

--- src/assets/covers/index.ts
+++ src/assets/covers/index.ts
@@ -1,6 +1,8 @@
 import { StaticImageData } from "next/image";
 import cruzadinhaCover from "./cruzadinhas-cover.png";
+import nonogramCover from "./nonogram-cover.png";
 
 export const covers: Record<string, StaticImageData> = {
-    "cruzadinhas": cruzadinhaCover
+    "cruzadinhas": cruzadinhaCover,
+    "nonogram": nonogramCover
 }
