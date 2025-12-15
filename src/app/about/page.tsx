b24fd21dfa1abe8cf2ce2e8d70082154bde600f1
João Vitor RM <67126712+joaovitorrm@users.noreply.github.com>
2025-12-15 14:07:52 +0000

    Inicio pagina sobre

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,20 @@
+import styles from "./page.module.css"
+import Image from "next/image"
+import githubIcon from "@/assets/util/github-icon-white.png"
+
 export default function About() {
     return (
-        <main>
-            <h1>About</h1>
+        <main className={styles["main-about"]}>
+            <section className={styles["about-section"]}>
+                <article className={styles["about-text"]}>
+                    Alguns jogos e projetos que desenvolvi utilizando Next/React para me desafiar e me divertir.
+                </article>
+                <article className={styles["socials"]}>
+                    <a href="https://github.com/joaovitorrm" target="_blank">
+                        <Image src={githubIcon} alt="github" fill={true} />
+                    </a>
+                </article>
+            </section>
         </main>
     )
 }
